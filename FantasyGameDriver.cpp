6a2c869d0624577a7ecad6405f5c91ede764acc4
Justin Henricks <justinhenricks@gmail.com>
2014-05-01 14:56:50 -0400

    Refactored battle arena

--- FantasyGameDriver.cpp
+++ FantasyGameDriver.cpp
@@ -6,8 +6,7 @@
 #include "stdafx.h"
 #include <vector>
 
-int battleArena(Creature &creature1, Creature &creature2);
-
+void battleArena(Creature &creature1, Creature &creature2);
 int main(){
 	srand ( time(NULL) ); //this gives different results every time without it, same sequence of numbers are always generated
 	std::vector<Creature*> creatureList;
@@ -28,38 +27,37 @@ int main(){
 	elf = new Elf(100, 100);
 	creatureList.push_back(elf);
 
-	int result = 3; //my "status" variable, 3 = keep going
-	while(result == 3){
-		result = battleArena(*creatureList[3], *human);
-	}
+	battleArena(*creatureList[3], *human);
 
 	return 0;
 }
 
 //My battle arena!!
-int battleArena(Creature &creature1, Creature &creature2){
-	cout << creature1.getSpecies() << " 1 has " << creature1.getHitPoints() << " hits left" << endl;
-	cout << creature2.getSpecies() << " 2 has " << creature2.getHitPoints() << " hits left" << endl;
+void battleArena(Creature &creature1, Creature &creature2){
+	cout << creature1.getSpecies() << " has " << creature1.getHitPoints() << " hits to start" << endl;
+	cout << creature2.getSpecies() << " has " << creature2.getHitPoints() << " hits to start" << endl;
+	cout << "\n";
+	while(creature1.getHitPoints() > 0 && creature2.getHitPoints() > 0){
+		creature1.setHitPoints(creature1.getHitPoints() - creature2.getDamage());
+		creature2.setHitPoints(creature2.getHitPoints() - creature1.getDamage());
 
-	int creature1DamageDone = creature1.getDamage();
-	int creature2DamageDone = creature2.getDamage();
+		if(creature1.getHitPoints() <= 0 && creature2.getHitPoints() <= 0){
+			cout << "\n";
+			cout << "the result is a tie!" << endl;
+		}
+		else if(creature1.getHitPoints() > 0 && creature2.getHitPoints() <= 0){
+			cout << "\n";
+			cout << creature1.getSpecies() << " 1 wins!!" << endl;
+		}
+		else if(creature1.getHitPoints() <= 0 && creature2.getHitPoints() > 0){
+			cout << "\n";
+			cout << creature2.getSpecies() << " 2 wins!!" << endl;
+		}
+		else{
+			cout << "\n";
+			cout << creature1.getSpecies() << " has " << creature1.getHitPoints() << " hits left" << endl;
+			cout << creature2.getSpecies() << " has " << creature2.getHitPoints() << " hits left" << "\n" << endl;
+		}
+	}
 
-	creature1.setHitPoints(creature1.getHitPoints() - creature2DamageDone);
-	creature2.setHitPoints(creature2.getHitPoints() - creature1DamageDone);
-
-	if(creature1.getHitPoints() <= 0 && creature2.getHitPoints() <= 0){
-		cout << "the result is a tie!" << endl;
-		return 0; //this is result of tie
-	}
-	else if(creature1.getHitPoints() > 0 && creature2.getHitPoints() <= 0){
-		cout << creature1.getSpecies() << " 1 wins!!" << endl;
-		return 1; //this is result of Creature 1 winning!!
-	}
-	else if(creature1.getHitPoints() <= 0 && creature2.getHitPoints() > 0){
-		cout << creature2.getSpecies() << " 2 wins!!" << endl;
-		return 2; //this is result of Creature 2 winning!!
-	}
-	else{
-		return 3; //this is result for looping
-	}
 }
